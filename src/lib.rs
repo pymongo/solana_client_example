c1866485430fd2a3265f13c9a1fd6a837cc26a1a
wuaoxiang <os.popen@gmail.com>
2024-07-16 18:48:07 +0800

    add solana invoke_program example

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+use solana_sdk::signature::Keypair;
+
+pub fn keypair() -> Keypair {
+    let home = std::env::var("HOME").unwrap();
+    let key_str = std::fs::read_to_string(format!("{home}/.config/solana/id.json")).unwrap();
+    let key: Vec<u8> = serde_json::from_str(&key_str).unwrap();
+    Keypair::from_bytes(&key).unwrap()
+}
---
+++ typescripts/invoke_program.ts
@@ -0,0 +1,37 @@
+import {
+    Connection,
+    PublicKey,
+    Keypair,
+    Transaction,
+    sendAndConfirmTransaction,
+    TransactionInstruction
+} from '@solana/web3.js';
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+dotenv.config();
+(async () => {
+    const connection = new Connection('https://api.devnet.solana.com', 'confirmed');
+    // Read the keypair from the file
+    const keypairPath = `${process.env.HOME}/.config/solana/id.json`;
+    const keypairData = JSON.parse(fs.readFileSync(keypairPath, 'utf8'));
+    const payer = Keypair.fromSecretKey(new Uint8Array(keypairData));
+    const programId = new PublicKey('6jL67XKqEVWPBZEmMT8AuhTP4zJefpPs48BmV3JLxncR');
+
+    // Create an instruction to call your program
+    const instruction = new TransactionInstruction({
+        // our program only logs hello world and doesn't need any accounts.
+        keys: [{pubkey: payer.publicKey, isSigner: true, isWritable: true}],
+        programId,
+        data: Buffer.alloc(0)
+    });
+    // Create a transaction
+    const transaction = new Transaction().add(instruction);
+    // Sign and Send the transaction
+    console.log("before send tx");
+    const signature = await sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [payer]
+    );
+    console.log('signature/txhash:', signature);
+})();
