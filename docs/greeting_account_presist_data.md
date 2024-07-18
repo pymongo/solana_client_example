20397d68afeba90ec46612a8d3073f70e3652a70
wuaoxiang <os.popen@gmail.com>
2024-07-18 09:06:33 +0800

    add greeting_account.rs

--- docs/greeting_account_presist_data.md
+++ docs/greeting_account_presist_data.md
@@ -13,3 +13,35 @@
          data: Buffer.alloc(0)
      });
 ```
+
+## signer 问题
+
+```
+AccountNotFound: pubkey=65hH53vj34oG6DGdFePftj7mMeiM3uQtvPLQNPUnSAkV
+thread 'main' panicked at /home/w/.cargo/registry/src/rsproxy.cn-0dccff568467c15b/solana-sdk-2.0.2/src/transaction/mod.rs:733:13:
+Transaction::sign failed with error NotEnoughSigners
+```
+
+full backtrace
+
+```
+AccountNotFound: pubkey=65hH53vj34oG6DGdFePftj7mMeiM3uQtvPLQNPUnSAkV
+thread 'main' panicked at /home/w/.cargo/registry/src/rsproxy.cn-0dccff568467c15b/solana-sdk-2.0.2/src/transaction/mod.rs:733:13:
+Transaction::sign failed with error NotEnoughSigners
+stack backtrace:
+   0: rust_begin_unwind
+             at /rustc/07dca489ac2d933c78d3c5158e3f43beefeb02ce/library/std/src/panicking.rs:645:5
+   1: core::panicking::panic_fmt
+             at /rustc/07dca489ac2d933c78d3c5158e3f43beefeb02ce/library/core/src/panicking.rs:72:14
+   2: solana_sdk::transaction::Transaction::sign
+             at /home/w/.cargo/registry/src/rsproxy.cn-0dccff568467c15b/solana-sdk-2.0.2/src/transaction/mod.rs:733:13
+   3: solana_sdk::transaction::Transaction::new
+             at /home/w/.cargo/registry/src/rsproxy.cn-0dccff568467c15b/solana-sdk-2.0.2/src/transaction/mod.rs:378:9
+   4: solana_sdk::transaction::Transaction::new_signed_with_payer
+             at /home/w/.cargo/registry/src/rsproxy.cn-0dccff568467c15b/solana-sdk-2.0.2/src/transaction/mod.rs:534:9
+   5: greeting_account::main
+             at ./examples/greeting_account.rs:55:47
+   6: core::ops::function::FnOnce::call_once
+             at /rustc/07dca489ac2d933c78d3c5158e3f43beefeb02ce/library/core/src/ops/function.rs:250:5
+note: Some details are omitted, run with `RUST_BACKTRACE=full` for a verbose backtrace.
+```
---
+++ examples/greeting_account.rs
@@ -0,0 +1,94 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_client::client_error::ClientErrorKind;
+use solana_client::rpc_client::RpcClient;
+use solana_client::rpc_request::RpcError;
+use solana_sdk::{
+    commitment_config::CommitmentConfig,
+    instruction::{AccountMeta, Instruction},
+    pubkey::Pubkey,
+    signature::Signer,
+    system_instruction::create_account_with_seed,
+    transaction::Transaction,
+};
+/// Define the type of state stored in accounts
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct GreetingAccount {
+    /// number of greetings
+    pub counter: u32,
+}
+fn main() {
+    dotenv::dotenv().unwrap();
+    std::env::set_var("RUST_BACKTRACE", "1");
+    let client = RpcClient::new_with_commitment(
+        "https://api.devnet.solana.com".to_string(),
+        CommitmentConfig::confirmed(),
+    );
+    let payer = solana_client_example::keypair();
+    let program_id: Pubkey = std::env::var("greeting_hello").unwrap().parse().unwrap();
+    let seed = "hello_rust";
+    let program_data_pubkey = Pubkey::create_with_seed(&payer.pubkey(), seed, &program_id).unwrap();
+    let data = GreetingAccount { counter: 0 };
+    let mut buffer = Vec::new();
+    data.serialize(&mut buffer).unwrap();
+    let size = buffer.len();
+
+    match client.get_account(&program_data_pubkey) {
+        Ok(_acc) => {}
+        Err(err) => {
+            match err.kind {
+                ClientErrorKind::RpcError(err) => {
+                    match err {
+                        RpcError::ForUser(err) => {
+                            // AccountNotFound: pubkey=65hH53vj34oG6DGdFePftj7mMeiM3uQtvPLQNPUnSAkV
+                            eprintln!("{err}");
+                            let lamports =
+                                client.get_minimum_balance_for_rent_exemption(size).unwrap();
+                            let ix = create_account_with_seed(
+                                &payer.pubkey(),
+                                &payer.pubkey(),
+                                &program_data_pubkey,
+                                seed,
+                                lamports,
+                                size as u64,
+                                &program_id,
+                            );
+                            let transaction = Transaction::new_signed_with_payer(
+                                &[ix],
+                                Some(&payer.pubkey()),
+                                &[&payer],
+                                client.get_latest_blockhash().unwrap(),
+                            );
+                            let txhash = client.send_and_confirm_transaction(&transaction).unwrap();
+                            println!("create_account_with_seed {txhash}");
+                        }
+                        _ => panic!("{err}"),
+                    }
+                }
+                _ => panic!("{err}"),
+            }
+        }
+    }
+
+    let query_account = || {
+        let data = client.get_account_data(&program_data_pubkey).unwrap();
+        let data = GreetingAccount::deserialize(&mut data.as_ref()).unwrap();
+        println!("{}", data.counter);
+    };
+    query_account();
+    let instruction = Instruction {
+        program_id,
+        accounts: vec![AccountMeta::new(payer.pubkey(), true)],
+        data: Vec::new(),
+    };
+    let transaction = Transaction::new_signed_with_payer(
+        &[instruction],
+        Some(&payer.pubkey()),
+        &[&payer],
+        client.get_latest_blockhash().unwrap(),
+    );
+    match client.send_and_confirm_transaction(&transaction) {
+        Ok(signature) => println!("Transaction sent with signature: {}", signature),
+        Err(err) => eprintln!("Error sending transaction: {:?}", err),
+    }
+    query_account();
+}
