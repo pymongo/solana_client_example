567c236567246f68d6552c8375532f6fee3720a3
wuaoxiang <os.popen@gmail.com>
2024-08-20 18:11:00 +0800

    golang borsh decode

--- golang/go.mod
+++ golang/go.mod
@@ -3,17 +3,19 @@ module golang
 go 1.22.4
 
 require (
+	github.com/gagliardetto/binary v0.8.0
 	github.com/gagliardetto/solana-go v1.11.0
 	github.com/joho/godotenv v1.5.1
 )
 
 require (
 	filippo.io/edwards25519 v1.0.0-rc.1 // indirect
+	github.com/andres-erbsen/clock v0.0.0-20160526145045-9e14626cd129 // indirect
 	github.com/blendle/zapdriver v1.3.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.9.0 // indirect
-	github.com/gagliardetto/binary v0.8.0 // indirect
 	github.com/gagliardetto/treeout v0.1.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/logrusorgru/aurora v2.0.3+incompatible // indirect
@@ -28,8 +30,10 @@ require (
 	go.mongodb.org/mongo-driver v1.11.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/ratelimit v0.2.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
 )
--- golang/greeting_account_borsh_serialize/main.go
+++ golang/greeting_account_borsh_serialize/main.go
@@ -1,13 +1,25 @@
+/*
+go get github.com/gagliardetto/solana-go@v1.11.0
+go get github.com/gagliardetto/solana-go/rpc@v1.11.0
+go get github.com/gagliardetto/binary@v0.8.0
+*/
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/gagliardetto/solana-go"
+	"github.com/gagliardetto/solana-go/rpc"
 	"github.com/joho/godotenv"
+	bin "github.com/gagliardetto/binary"
 )
+
+type GreetingHello struct {
+	Counter uint32
+}
 
 func main() {
 	log.SetFlags(log.Lmicroseconds | log.Lshortfile)
@@ -21,11 +33,29 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	// fmt.Sprintf("%s/.config/solana/id.json", os.Getenv("HOME"))
 	payer, err := solana.PrivateKeyFromSolanaKeygenFile(fmt.Sprintf("%s/.config/solana/id.json", os.Getenv("HOME")))
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	key, err := solana.CreateWithSeed(payer.PublicKey(), "hello_rust", program_id)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client := rpc.New(rpc.DevNet.RPC)
+	data, err := client.GetAccountInfo(context.Background(), key)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	borshDec := bin.NewBorshDecoder(data.GetBinary())
+	var counter GreetingHello
+	err = borshDec.Decode(&counter)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(counter.Counter)
+	// client.GetLatestBlockhash(context.Background(), rpc.CommitmentConfirmed)
 
 	log.Println(program_id, payer)
 }
